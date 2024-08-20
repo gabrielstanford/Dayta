7e7a5675cec30f4d759736bca3895cb7c557c3f5
gabrielstanford <166243116+gabrielstanford@users.noreply.github.com>
2024-08-19 18:11:02 -0700

    Created modal for describing recommendations, started implementing custom recommendations.Will submit a build to testflight.

--- app/(tabs)/recommendations.tsx
+++ app/(tabs)/recommendations.tsx
@@ -1,29 +1,57 @@
-import { StyleSheet, View, Dimensions } from 'react-native';
-import { HelloWave } from '@/components/HelloWave';
+import { StyleSheet, View, Dimensions, TouchableOpacity, Text } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
+import {useState, useEffect} from 'react'
+import FetchDayActivities from '@/Data/FetchDayActivities';
+import { useAuth } from '@/contexts/AuthContext';
+import { Activity } from '@/Types/ActivityTypes';
+import { useCustomSet } from '@/Data/CustomSet';
+import { useAppContext } from '@/contexts/AppContext';
+import RecDescribeModal from '@/components/RecDescribeModal';
 //import firestore from '@react-native-firebase/firestore'
-import { format } from 'date-fns-tz';
 
 const { width, height } = Dimensions.get('window');
 const buttonWidth = width/6.25
 
-// Function to convert Unix timestamp to local time based on the user's timezone
-const convertToLocalTime = (timestamp: number, timezone: string) => {
-  const date = new Date(timestamp * 1000); // Convert Unix timestamp to JavaScript Date object
-  return format(date, 'yyyy-MM-dd HH:mm:ssXXX', { timeZone: timezone });
-};
-
 //pass in activities, times
 //convert to local time
 export default function Recommendations() {
+  const [rec1, setRec1] = useState<string>("")
+  const {state} = useCustomSet();
+  const entertainmentSpeel = `We know that entertaining activities can feel good in the moment, and a bit of it is fine, but in excessive amounts these will have detrimental effects on the health of your dopamine system. In other words, in the long term you will be more sad and stressed if you continue like this. But don't worry, we're here to help!`
+  const {justActivities} = useAppContext();
+  const {tagDurationSum} = state
+  const [todayActivities, setTodayActivities] = useState<Activity[]>([]);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const {user} = useAuth();
+  useEffect(() => {
+    FetchDayActivities(user, 0, justActivities, setTodayActivities)
+  }, [justActivities])
+
+  useEffect(() => {
+    const entertainment = tagDurationSum.find(tag => tag.text=="Entertainment")
+    if(entertainment) {
+      if(entertainment.totalDuration>6) {
+        console.log("excessive entertainment")
+        const timeSpent = Math.round(entertainment.totalDuration)
+        setRec1(`You spend about ${timeSpent} hours on entertaining activities daily. Let's take tangible, realistic steps to fix that and make you happier in the long run.`)
+      }
+    }
+  }, [todayActivities])
 
   return (
     <View style={styles.layoutContainer}>
+      <RecDescribeModal visible={modalVisible} onClose={() => setModalVisible(false)} speel={entertainmentSpeel} />
       <View style={styles.titleContainer}>
         <ThemedText type="titleText" style={{fontSize: width/12}}>Recommendations</ThemedText>
       </View>
       <View style={styles.stepContainer}>
-        <ThemedText type="journalText">Rec 1: Wake Up 1 Hour Later</ThemedText>
+        <ThemedText type="journalText">{rec1}</ThemedText>
+        
+        <TouchableOpacity onPress={() => setModalVisible(true)}>
+          <View style={styles.detailsButton}>
+          <Text>Details</Text>
+          </View>
+          </TouchableOpacity>
       </View>
       <View style={styles.stepContainer}>
         <ThemedText type="journalText">Rec 2: Go For A Walk Tomorrow Morning</ThemedText>
@@ -49,6 +77,9 @@ const styles = StyleSheet.create({
   titleContainer: {
     alignItems: 'center',
     padding: 10,
+  },
+  detailsButton: {
+    backgroundColor: 'red'
   },
   stepContainer: {
     padding: 8,
